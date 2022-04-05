c5470b984443244cec500c0e4f4a15227b4b6a5f
Raffaele Cataldo <kemanicataldo@gmail.com>
2022-04-05 17:06:52 +0200

    added "Output" wrapper component and asynccomponent

--- src/components/Greeting.jsx
+++ src/components/Greeting.jsx
@@ -1,17 +1,17 @@
-import { useState } from 'react'
+import { useState } from "react";
+import Output from "./Output";
 
 const Greeting = () => {
-  
   const [text, setText] = useState(false);
   const changeTextHandler = () => {
-    setText(true)
-  }
-  
+    setText(true);
+  };
+
   return (
     <div>
       <h2>Hello World</h2>
-      {!text && <p>Good to see you!</p> }
-      {text && <p>Bye Bye!</p>}
+      {!text && <Output>Good to see you!</Output>}
+      {text && <Output>Bye Bye!</Output>}
       <button onClick={changeTextHandler}>Change Text</button>
     </div>
   );
