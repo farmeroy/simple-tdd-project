23ef2ee5db37cf8e4ce1f0b11cc463afe195df45
Raffaele Cataldo <kemanicataldo@gmail.com>
2022-04-06 10:37:07 +0200

    imported <Form />

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Greeting from './components/Greeting'
+import Form from './components/Form'
 
 function App() {
   return (
     <div className="App">
       <Greeting />
+      <Form />
     </div>
   );
 }
